ec00a081a5d118b2c4905b03d8c0e2ce110fdcd3
aashazfarhan <87229733+aashazfarhan@users.noreply.github.com>
2021-07-10 16:53:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var firstName= Aashaz;
+var lastName= Farhan;
+var fullName= firstName.concat(lastName);
+console.log(fullName);
+
+var a = 3;
+var b  = 9;
+
+var add = a + b;
+console.log(add);
+
+var minus = b - a;
+console.log(minus);
+
+var multi = a * b;
+console.log(multi);
+
+var divi = b / a;
+console.log(divi);
